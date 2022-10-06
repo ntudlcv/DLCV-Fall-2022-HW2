f6cd482cec31f9b0d810192b32bae7aedf3a86f4
alun <come880412@gmail.com>
2022-10-06 18:08:08 +0800

    push code

---
+++ get_dataset.sh
@@ -0,0 +1,8 @@
+# Download dataset
+gdown 1YxkObGDlqZM0-9Zq-QMjk7q1vND4UJl3 -O hw2_data.zip
+# Download classifier.pth
+gdown 1sHqg5W0rcX85ldhL_PJEya3yV2thu3LD
+
+# Unzip the downloaded zip file
+unzip ./hw2_data.zip -d ./
+rm hw2_data.zip
